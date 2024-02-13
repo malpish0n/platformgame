1a221c4354e43690c98f968ad7d5cee0e7a1725f
Czolsonn <polushubert@gmail.com>
2024-02-13 03:35:25 +0100

    13.02.2024 03:34
    Ślizg po ścianie - działa bardzo dobrze
    Skok po ścianie - lekko zbugowany
    CameraFollow - lekkie zmiany optymalizacyjne pod platformówkę

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -31,6 +31,23 @@ public class Movement : MonoBehaviour
     public float jumpStartTime;
     bool offtheGround;
 
+    bool isWallSliding;
+    public float wallSlidingSpeed;
+    public GameObject wallDetect;
+    public float wallDetectRadius;
+    public LayerMask wallLayer;
+
+    public bool isWallJumping;
+    float wallJumpDirection;
+    bool wallJumpDirBol;
+    public float wallJumpTime;
+    public Vector2 wallJumpVelocity;
+    float wallJumpCounter;
+    public float wallJumpDuration;
+    float wallJumpDurationPass;
+
+
+
 
 
 
@@ -53,19 +70,25 @@ public class Movement : MonoBehaviour
         //zmienna isLanding sprawdzajaca czy gracz zbliza sie do l¹dowania
         isLanding = Physics2D.OverlapCircle(landingCheck.position, landingCheckRadius, groundLayer);
 
-        Running();
+        if (!isWallJumping)
+        {
+            Running();
+        }
 
         //mechanizm poruszania right/left
         move = Input.GetAxisRaw("Horizontal");
-        rig.velocity = new Vector2(move * speed, rig.velocity.y);
 
         Jump();
+        WallSliding();
+        WallJumping(); //lekki bug gdy bardzo szybko klikniesz np. na lewej œcianie d najpierw i spacjê to mo¿e skoczyæ w z³¹ stronê. Jak naprawiæ (nie wiem)
 
     }
     private void FixedUpdate()
     {
-
-        rig.velocity = new Vector2(move * speed, rig.velocity.y);
+        if (!isWallJumping)
+        {
+            rig.velocity = new Vector2(move * speed, rig.velocity.y);
+        }
 
     }
 
@@ -141,6 +164,74 @@ public class Movement : MonoBehaviour
             anim.SetBool("Landing", true);
             landing = false;
         }
+    }
+    bool isWall()
+    {
+        return Physics2D.OverlapCircle(wallDetect.transform.position, wallDetectRadius, wallLayer);
+    }
+    
+
+    void WallSliding()
+    {
+        if(isWall() && !isGrounded && move!= 0)
+        {
+            isWallSliding = true;
+            rig.velocity = new Vector2(rig.velocity.x, Mathf.Clamp(rig.velocity.y, -wallSlidingSpeed, float.MaxValue));
+
+        }
+        
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    void WallJumping() //lekki bug gdy bardzo szybko klikniesz np. na lewej œcianie d najpierw i spacjê to mo¿e skoczyæ w z³¹ stronê. Jak naprawiæ (nie wiem)
+    {
+        if (isWallSliding)
+        {
+            wallJumpDirBol = !sprite.flipX;
+            wallJumpCounter = wallJumpTime;
+            if (wallJumpDirBol)
+            {
+                wallJumpDirection = -1;
+            }
+            else
+            {
+                wallJumpDirection = 1;
+            }
+        }
+        else
+        {
+            wallJumpCounter -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Jump") && wallJumpCounter > 0)
+        {
+            isWallJumping = true;
+            rig.velocity = new Vector2(wallJumpDirection*wallJumpVelocity.x, wallJumpVelocity.y);
+            wallJumpCounter = 0;
+            if (sprite.flipX != wallJumpDirBol)
+            {
+                sprite.flipX = !sprite.flipX;
+            }
+            wallJumpDurationPass = wallJumpDuration;
+
+
+        }
+        if (isWallJumping)
+        {
+            wallJumpDurationPass -= Time.deltaTime;
+            if(wallJumpDurationPass < 0)
+            {
+                StopWallJumping();
+            }
+        }
+
+    }
+
+    void StopWallJumping()
+    {
+        isWallJumping = false;
     }
 }
 
